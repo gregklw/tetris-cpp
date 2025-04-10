53d10ccbecdba632def9e31d20898048cf7bf1b4
gwongrp4k <gwong@realprogramming.com>
2025-04-10 14:14:13 -0400

    refactor: more UI code

--- header/GameUI.h
+++ header/GameUI.h
@@ -30,47 +30,54 @@ extern GameArea gameArea;
 /// </summary>
 sf::Vector2f holdBoxLocation;
 
+//Score Display
+sf::Text scoreText(font);
 
+inline void setupText(sf::Text& text, const sf::String& startString, unsigned int characterSize, sf::Color textFillColor = sf::Color::White, uint32_t style = sf::Text::Regular)
+{
+	// set the string to display
+	text.setString(startString);
 
+	// set the character size
+	text.setCharacterSize(characterSize); // in pixels, not points!
+
+	// set the color
+	text.setFillColor(textFillColor);
+
+	// set the text style
+	text.setStyle(style);
+}
+
+inline void setupBox(sf::RectangleShape& shape, sf::Vector2f size, float outlineThickness, sf::Color outlineColor, sf::Color fillColor)
+{
+	// set the string to display
+	shape.setSize(size);
+
+	// set the character size
+	shape.setOutlineThickness(outlineThickness); // in pixels, not points!
+
+	// set the color
+	shape.setFillColor(fillColor);
+	shape.setOutlineColor(outlineColor);
+}
 
 inline void initUI()
 {
-	// set the string to display
-	holdBoxText.setString("Hold");
+	setupText(holdBoxText, "Hold", 24, sf::Color::Red, sf::Text::Bold);
 
-	// set the character size
-	holdBoxText.setCharacterSize(24); // in pixels, not points!
-
-	// set the color
-	holdBoxText.setFillColor(sf::Color::Red);
-
-	// set the text style
-	holdBoxText.setStyle(sf::Text::Bold);
 	sf::FloatRect holdBoxTextRect = holdBoxText.getGlobalBounds();
 	holdBoxText.setPosition(gameAreaPosition - sf::Vector2f(holdBoxTextRect.size.x * 1.75, 0));
 
-	
-
 	sf::Vector2f padding = sf::Vector2f(0, 10);
-
-	holdBoxShape.setSize(sf::Vector2f(125, 125));
-	holdBoxShape.setOutlineThickness(-5);
-	holdBoxShape.setOutlineColor(sf::Color::Red);
-	holdBoxShape.setFillColor(sf::Color::Transparent);
+	setupBox(holdBoxShape, sf::Vector2f(125, 125), -5, sf::Color::Red, sf::Color::Transparent);
 	holdBoxShape.setPosition(sf::Vector2f(gameAreaPosition.x - holdBoxShape.getSize().x, gameAreaPosition.y) + sf::Vector2f(0, holdBoxTextRect.size.y) + padding);
 	holdBoxLocation = holdBoxShape.getPosition() + sf::Vector2f(holdBoxShape.getSize().x / 8, holdBoxShape.getSize().y / 4);
 
-
-	nextShapeBox.setSize(sf::Vector2f(125, 450));
-	nextShapeBox.setOutlineColor(sf::Color::White);
-	nextShapeBox.setOutlineThickness(-5);
-	nextShapeBox.setFillColor(sf::Color::Transparent);
-
+	setupBox(nextShapeBox, sf::Vector2f(125, 450), -5, sf::Color::White, sf::Color::Transparent);
 	sf::Vector2f gameAreaRightAnchor = gameAreaPosition + sf::Vector2f(gameAreaDimensions.x * gameAreaBlockSize, 0);
-
-	nextText.setPosition(gameAreaRightAnchor + sf::Vector2f(125/3, 0));
-	nextText.setString("Next");
-	nextText.setCharacterSize(24);
+	
+	setupText(nextText, "Next", 24);
+	nextText.setPosition(gameAreaRightAnchor + sf::Vector2f(125 / 3, 0));
 
 	sf::FloatRect nextTextRect = nextText.getGlobalBounds();
 	nextShapeBox.setPosition(gameAreaRightAnchor + sf::Vector2f(0, nextTextRect.size.y) + padding);
@@ -92,7 +99,7 @@ inline void displayGameUI(sf::RenderWindow& window)
 	window.draw(nextText);
 	window.draw(nextShapeBox);
 	if (heldPiecePointer != nullptr) heldPiece.update(window);
-	
+
 	int verticalPaddingNextPreview = 20;
 	for (int i = 0; i < gameArea.nextPieces.size(); i++)
 	{
